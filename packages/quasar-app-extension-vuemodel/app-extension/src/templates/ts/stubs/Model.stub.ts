90c6dc3b554c239778cdd77135baa91c48dab926
Luke Diebold <luke@ldiebold.com>
2025-03-27 15:28:01 +1100

    stub attr should be Attr

--- packages/quasar-app-extension-vuemodel/app-extension/src/templates/ts/stubs/Model.stub.ts
+++ packages/quasar-app-extension-vuemodel/app-extension/src/templates/ts/stubs/Model.stub.ts
@@ -1,17 +1,17 @@
 import { Model } from 'pinia-orm'
-import { Attr, Str, Uid, BelongsTo } from 'pinia-orm/dist/decorators'
+import { Attr, Uid, BelongsTo } from 'pinia-orm/dist/decorators'
 import { User } from 'src/models/User'
 
 export class {{ resourceClass }} extends Model {
   static override entity = '{{ resourceTable }}'
 
   @Uid() declare id: string
-  @Str(null) declare created_at: string
-  @Str(null) declare updated_at: string
+  @Attr(null) declare created_at: string
+  @Attr(null) declare updated_at: string
 
   // Fields
   @Attr() declare user_id: string
-  @Attr() declare some_field: string // 🤿
+  @Attr() declare some_field: string | null // 🤿
 
   // Relationships
   @BelongsTo(() => User, 'user_id') declare user: User | null
